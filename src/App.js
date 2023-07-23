169177700dee018605b958bddedac194943db1ec
Hamouda-Yasmine <hamouda.yasmine9@gmail.com>
2023-07-23 22:45:47 +0200

    added some icones

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-
+import { Navbar,Container, Nav, Row, Col} from 'react-bootstrap';
+import { faAddressCard, faDownload, faGears, faHouse, faLaptopCode, faUserGraduate } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 import Home from './components/Home';
 import Education from './components/Education';
@@ -6,10 +7,15 @@ import Skills from './components/Skills';
 import Mywork from './components/Mywork';
 import Contacts from './components/Contacts';
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link,Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import logo from './assets/img/logo.svg';
+import navIcon1 from './assets/img/nav-icon1.svg';
+import navIcon2 from './assets/img/nav-icon2.svg';
+import navIcon3 from './assets/img/nav-icon3.svg';
 function App() {
+
   const [activeRoute,setActiveRoute]=useState();
 
   useEffect(()=>{
@@ -24,30 +30,30 @@ function App() {
     */ 
  
       
-       <BrowserRouter>
-       <nav className='m-1 p-1 border border-info navbar navbar-expand-lg navbar-light bg-light'>
-        <ul className='nav na-pills'>
+       <BrowserRouter >
+      <Navbar expand="md">
+            <Container>
+        <Navbar.Brand>
+        <p className='m-1'>Hamouda yasmine</p>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className='navbar-toggler-icon'></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" onClick={()=>setActiveRoute("")} className={activeRoute===""?'active navbar-link  m-1':'navbar-link m-1'}> <FontAwesomeIcon icon={faHouse} /> Home</Nav.Link>
+            <Nav.Link as={Link} to="/education"onClick={()=>setActiveRoute("education")} className={activeRoute==="education"?'active navbar-link m-1 ':'navbar-link m-1'}><FontAwesomeIcon icon={faUserGraduate} /> Education</Nav.Link>
+            <Nav.Link as={Link} to="/skills"onClick={()=>setActiveRoute("skills")} className={activeRoute==="skills"?'active navbar-link  m-1':'navbar-link m-1'}><FontAwesomeIcon icon={faGears} /> Skills</Nav.Link>
+            <Nav.Link as={Link} to="/mywork"onClick={()=>setActiveRoute("mywork")} className={activeRoute==="mywork"?'active navbar-link  m-1':'navbar-link m-1'}><FontAwesomeIcon icon={faLaptopCode} /> Projects</Nav.Link>
+            <Nav.Link as={Link} to="/contacts"onClick={()=>setActiveRoute("contacts")} className={activeRoute==="contacts"?'active navbar-link m-1 ':'navbar-link m-1'}><FontAwesomeIcon icon={faAddressCard} /> Contact</Nav.Link>
+            <Nav.Link className='btn btn-info m-1'>download my CV <FontAwesomeIcon icon={faDownload} /></Nav.Link>
+          </Nav>
          
-          <li>
-          <Link to="/" onClick={()=>setActiveRoute("")} className={activeRoute==""?'btn btn-info m-1 ':'btn btn-outline-info  m-1'}> Home</Link>
-          </li>
-          <li>
-          <Link to="/education" onClick={()=>setActiveRoute("education")} className={activeRoute=="education"?'btn btn-info m-1 ':'btn btn-outline-info  m-1'}> Education</Link>
-          </li>
-          <li>
-          <Link to="/skills" onClick={()=>setActiveRoute("skills")}className={activeRoute=="skills"?'btn btn-info m-1 ':'btn btn-outline-info  m-1'}> skills</Link>
-          </li>
-          <li>
-          <Link to="/mywork" onClick={()=>setActiveRoute("mywork")} className={activeRoute=="mywork"?'btn btn-info m-1 ':'btn btn-outline-info  m-1'}> My Work</Link>
-          </li>
-          <li>
-          <Link to="/contacts" onClick={()=>setActiveRoute("contacts")} className={activeRoute=="contacts"?'btn btn-info m-1 ':'btn btn-outline-info  m-1'}> Contacts</Link>
-          </li>
-        </ul>
-       
-
-       </nav>
-     
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+        
+      
  
          <Routes>
           <Route path='/' element={<Home />}></Route>
@@ -56,6 +62,25 @@ function App() {
           <Route path='/mywork' element={<Mywork/>}></Route>
           <Route path='/contacts' element={<Contacts/>}></Route>
          </Routes>
+
+         <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+         
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
 
       </BrowserRouter>
      
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import headerImg from "../assets/img/header-img.svg";
 
 function Home() {
   return (
-    <div>hello from home</div>
+    <section className='banner'>
+      <Container>
+      <Row className="aligh-items-center">
+        <Col xs={12} md={6} xl={7}>
+          <span className='tagline'>Welcome in to my Portfolio</span>
+
+
+        </Col>
+        <Col xs={12} md={6} xl={5}>
+        <img src={headerImg} alt="Header Img"/>
+        </Col>
+      </Row>
+      </Container>
+    </section>
   )
 }
 
