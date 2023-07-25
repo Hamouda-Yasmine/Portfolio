2af44d8a2eaca21ed99796d760f13552b9c51c0e
Hamouda-Yasmine <hamouda.yasmine9@gmail.com>
2023-07-25 19:06:42 +0200

    typing changes

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from './assets/img/logo.svg';
 import navIcon1 from './assets/img/nav-icon1.svg';
-import navIcon2 from './assets/img/nav-icon2.svg';
-import navIcon3 from './assets/img/nav-icon3.svg';
+
 function App() {
 
   const [activeRoute,setActiveRoute]=useState();
@@ -72,9 +71,8 @@ function App() {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="/"><img src={navIcon1} alt="Icon" /></a>
+          
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/desktop-workstation (2).png";
 
 function Home() {
+  const text='Hello there! I am Yasmine Hamouda, a passionate and creative software developer with a love for crafting elegant solutions. Welcome to my digital sanctuary, where I showcase my journey through the world of coding and design.';
+  const speed=100;
+  const [visibleText, setVisibleText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentIndex === text.length) {
+        clearInterval(interval); // Stop the typing effect when all text is shown
+      } else {
+        setVisibleText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }
+    }, speed);
+
+    return () => {
+      clearInterval(interval); // Clean up the interval on component unmount
+    };
+  }, [currentIndex, speed, text]);
   return (
-    <section className='banner'>
+    <section className='home'>
       <Container>
       <Row className="aligh-items-center">
-        <Col xs={12} md={6} xl={7}>
-          <span className='tagline'>Welcome in to my Portfolio.</span>
+        <Col xs={10} md={6} xl={6.5}>
+          <span className='tagline'>Welcome into my Portfolio.</span>
+          <h1> <span className='wrap'>{visibleText}</span></h1>
 
 
         </Col>
-        <Col xs={12} md={6} xl={5}>
+        <Col xs={10} md={5} xl={4}>
         <img src={headerImg} alt="Header Img"/>
         </Col>
       </Row>
