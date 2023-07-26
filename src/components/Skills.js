032fe5fac2538c17b8922161c3695342fcd33aeb
Hamouda-Yasmine <hamouda.yasmine9@gmail.com>
2023-07-26 23:14:17 +0200

    added progress bar in skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import  './Skills.css';
+
+
 
 function Skills() {
+ 
+  
   return (
-    <div> hello Skills</div>
+    <section className='skill'>
+      <Container>
+        <Row>
+          <Col>
+          <div className='skill-bx'>
+            <h2>
+             Back-end
+            </h2>
+            
+            <div className="progress-bar">
+         
+          <div className="progress" style={{ width: `${60}%` }}>
+            <span className="percentage">60%</span>
+          </div>
+        </div>
+        <div className="progress-bar">
+          <div className="progress" style={{ width: `${80}%` }}>
+            <span className="percentage">80%</span>
+          </div>
+        </div>
+           
+           
+          </div>
+          
+      
+          </Col>
+          <Col>
+          <div className='skill-bx'>
+            <h2>
+              Front-end
+            </h2>
+            <p>
+
+            </p>
+          
+           
+          </div>
+          </Col>
+          <Col>
+          <div className='skill-bx'>
+            <h2>
+              Qualities
+            </h2>
+            <p>blabla</p>
+          
+           
+          </div>
+          </Col>
+        </Row>
+    
+         
+      </Container>
+     
+    </section>
+   
   )
 }
 
