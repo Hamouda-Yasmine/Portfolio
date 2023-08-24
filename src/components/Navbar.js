495afeb3384d78706af933c04052e2742f417b53
Hamouda-Yasmine <hamouda.yasmine9@gmail.com>
2023-08-24 23:12:59 +0200

    some design changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import {  Container, Nav, Navbar } from 'react-bootstrap'
+import { Button} from '@mui/material'
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 import {  faDownload } from '@fortawesome/free-solid-svg-icons'
@@ -23,14 +24,16 @@ return (
         <span className='navbar-toggler-icon'></span>
       </Navbar.Toggle>
       <Navbar.Collapse id="basic-navbar-nav">
-       <Nav className="ms-auto">
+       <Nav className="ms-auto  me-auto d-flex align-items-center">
          <Nav.Link as={Link} to="/" onClick={()=>setActiveRoute("")} className={activeRoute===""?'active navbar-link  m-1':'navbar-link m-1'}> Home</Nav.Link>
          <Nav.Link as={Link} to="/education"onClick={()=>setActiveRoute("education")} className={activeRoute==="education"?'active navbar-link m-1 ':'navbar-link m-1'}> Education</Nav.Link>
          <Nav.Link as={Link} to="/skills"onClick={()=>setActiveRoute("skills")} className={activeRoute==="skills"?'active navbar-link  m-1':'navbar-link m-1'}> Skills</Nav.Link>
          <Nav.Link as={Link} to="/mywork"onClick={()=>setActiveRoute("mywork")} className={activeRoute==="mywork"?'active navbar-link  m-1':'navbar-link m-1'}> Projects</Nav.Link>
          <Nav.Link as={Link} to="/contacts"onClick={()=>setActiveRoute("contacts")} className={activeRoute==="contacts"?'active navbar-link m-1 ':'navbar-link m-1'}> Contact</Nav.Link>
-         <Nav.Link className='btn btn-info m-1'>download my CV <FontAwesomeIcon icon={faDownload} /></Nav.Link>
+         
+         
        </Nav>
+       <Button  color="secondary"> Download my CV<FontAwesomeIcon icon={faDownload} /></Button>
       </Navbar.Collapse>
    </Container>
    </Navbar>
